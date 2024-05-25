43abfbac33eb1a52d529627454e32110082413af
AnaCa <AnaCa@lazzuriana>
2024-05-25 03:42:01 -0300

    Adição da função de taxação na venda de criptomoedas no arquivo Taxa

--- src/Model/Taxa.java
+++ src/Model/Taxa.java
@@ -9,8 +9,7 @@ package Model;
  * @author AnaCa
  */
 public class Taxa {
- 
-    public double calculoTx(String siglaCripto, double valorCompra) {
+     public double calculoTx(String siglaCripto, double valorCompra) {
         return calcularTaxaCmp(siglaCripto, valorCompra);
     }
 
@@ -27,9 +26,32 @@ public class Taxa {
                 taxaCompra = 0.01;
                 break;
             default:
-                taxaCompra = 0.00;
+                taxaCompra = 0.000001;
+                break;
+        }
+        return valorCompra * taxaCompra;
+    }
+  public double calculoTxVenda(String siglaCripto, double valorCompra) {
+        return calcularTaxaVnd(siglaCripto, valorCompra);
+    }
+  
+      private double calcularTaxaVnd(String siglaCripto, double valorCompra) {
+        double taxaCompra;
+        switch (siglaCripto.toUpperCase()) {
+            case "BTC":
+                taxaCompra = 0.03;
+                break;
+            case "RPL":
+                taxaCompra = 0.01;
+                break;
+            case "ETH":
+                taxaCompra = 0.02;
+                break;
+            default:
+                taxaCompra = 0.00001;
                 break;
         }
         return valorCompra * taxaCompra;
     }
 }
+
